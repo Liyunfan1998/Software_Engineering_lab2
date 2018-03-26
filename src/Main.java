875baad44c09c404ba4d6d8073ae201e673fc282
liyunfan1998 <16302010002@fudan.edu.cn>
2018-03-26 18:16:17 +0800

    input error handling

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,18 @@
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.geometry.Insets;
+import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.*;
-import javafx.scene.text.Text;
+import javafx.scene.control.Tooltip;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
-
-import java.util.List;
-//import java.util.GregorianCalendar;
 
 public class Main extends Application {
     @Override
@@ -69,15 +69,26 @@ public class Main extends Application {
         HBox bottomBox = new HBox(10);
         Button bTypeSearch = new Button("输入查询");
         TextField textField = new TextField();
-        bTypeSearch.setOnAction(event -> {
+        bTypeSearch.setOnAction((ActionEvent event) -> {
             String date = textField.getText();
             if (DateUtil.isFormatted(date)) {
                 CalendarDate cd = new CalendarDate(date);
                 if (DateUtil.isValid(cd)) {
                     calendarPane.search(cd);
+                } else {
+//                    String p_message = "输入格式错误";
+//                    Alert _alert = new Alert(Alert.AlertType.CONFIRMATION,p_message,new ButtonType("取消", ButtonBar.ButtonData.NO),new ButtonType("确定", ButtonBar.ButtonData.YES));
+                    calendarPane.f_alert_informationDialog("ERROR", "输入日期不合法", primaryStage);
                 }
+            } else {
+                calendarPane.f_alert_informationDialog("ERROR", "输入格式错误", primaryStage);
+
             }
         });
+        final Tooltip tooltip = new Tooltip("请输入格式为 YYYY-MM-DD 的日期");
+        tooltip.setFont(new Font("Arial", 16));
+        textField.setTooltip(tooltip);
+
         bottomBox.getChildren().addAll(textField, bTypeSearch);
         bottomBox.setAlignment(Pos.CENTER);
 
@@ -107,192 +118,7 @@ public class Main extends Application {
 /**
  * Extend the Pane class that will create a calendar pane
  */
-class CalendarPane extends Pane {
-    //    private Calendar calendar;
-//    private CalendarDate calendarDate;
-    private Text calendarLabel;
-    private String monthName;
 
-    /**
-     * Default constructor
-     */
-    CalendarPane(CalendarDate calendarDate) {
-        calendarDate = DateUtil.getToday();
-//        calendarDate.setDay(1);
-        draw(calendarDate);
-    }
-
-    /**
-     * Advance the calendar by 1 month
-     */
-    void nextYear(CalendarDate calendarDate) {
-        simpleChangeYear(calendarDate, 1);
-    }
-
-    private void simpleChangeYear(CalendarDate calendarDate, int x) {
-        int y = calendarDate.getYear();
-        calendarDate.setYear(y + x);
-        draw(calendarDate);
-    }
-
-    public void search(CalendarDate calendarDate) {
-        draw(calendarDate);
-    }
-
-    void lastYear(CalendarDate calendarDate) {
-        simpleChangeYear(calendarDate, -1);
-    }
-
-    void nextMonth(CalendarDate calendarDate) {
-        int m = calendarDate.getMonth();
-        if (m == 12) {
-            m = 0;
-            int y = calendarDate.getYear();
-            calendarDate.setYear(y + 1);
-        }
-        calendarDate.setMonth(m + 1);
-        draw(calendarDate);
-    }
-
-    /**
-     * Decrement the calendar by 1 month
-     */
-    void lastMonth(CalendarDate calendarDate) {
-        int m = calendarDate.getMonth();
-        if (m == 1) {
-            m = 13;
-            int y = calendarDate.getYear();
-            calendarDate.setYear(y - 1);
-        }
-        calendarDate.setMonth(m - 1);
-        draw(calendarDate);
-    }
-
-    /**
-     * Set up the calendar pane and get the require objects
-     */
-    protected void draw(CalendarDate calendarDate) {
-//      先加高亮的逻辑部分,加在getCalendarGrid函数里
-        this.getChildren().clear();
-        // Create the border pane, then get the calendar header and body
-        BorderPane borderPane = new BorderPane();
-        borderPane.setTop(getCalendarHeader(calendarDate)); //OK
-        borderPane.setCenter(getCalendarGrid(calendarDate));    //Not ok
-
-        displayCalendar(borderPane);
-    }
-
-    /**
-     * Return the calendar pane
-     */
-    private void displayCalendar(BorderPane borderPane) {
-        this.getChildren().addAll(borderPane);
-    }
-
-    private void showToday() {
-        draw(DateUtil.getToday());
-    }
-
-    private Pane getCalendarGrid(CalendarDate calendarDate) {
-        // Variables
-        int day = calendarDate.getDay();
-        int dayOfWeek = calendarDate.getDayOfWeek();
-        int daysInMonth = DateUtil.getNumberOfDaysInMonth(calendarDate);
-
-        // Create the GridPane for the calendar
-        GridPane calendarGrid = new GridPane();
-        calendarGrid.setHgap(10.0);
-        calendarGrid.setVgap(5.0);
-        calendarGrid.setPadding(new Insets(10));
-
-        // Add the days of the week to the top row of the calendar grid
-        calendarGrid.addRow(0, new Text("Sunday"), new Text("Monday"),
-                new Text("Tuesday"), new Text("Wednesday"), new Text("Thursday"),
-                new Text("Friday"), new Text("Saturday"));
-
-        // Print the dates for the current month's calendar
-        //        Calendar lastMonth = (Calendar) calendar.clone();
-        List<CalendarDate> thisMonth = DateUtil.getDaysInMonth(calendarDate);
-        // Clone does not effect current calendar object
-        for (int i = 1; i <= daysInMonth; i++) {
-            CalendarDate cd = thisMonth.get(i - 1);
-            int columnIndex = cd.getDayOfWeek() % 7;
-            int rowIndex = DateUtil.getLineInPane(cd);
-            Button button = new Button(cd.getDay() + "");
-            button.setMinWidth(45);
-            if (i == day) {
-                button.setStyle("-fx-background-color: red");
-            }
-            calendarGrid.add(button, columnIndex, rowIndex);
-        }
-
-        // Print the dates for the previous month on the current calendar
-
-        // Print the dates for the next month on the current calendar
-        return calendarGrid;
-    }
-
-    /**
-     * Create the label pane and display current month and year
-     */
-    private Pane getCalendarHeader(CalendarDate calendarDate) {
-        calendarLabel = new Text(getMonthName(calendarDate.getMonth()) + ", " + calendarDate.getYear());
-        HBox labelBox = new HBox();
-        labelBox.getChildren().addAll(calendarLabel);
-        labelBox.setAlignment(Pos.CENTER);
-
-        return labelBox;
-    }
-
-    /**
-     * Return month name in English
-     */
-    private String getMonthName(int month) {
-        // String monthName = null;
-
-        switch (month) {
-            case 1:
-                monthName = "January";
-                break;
-            case 2:
-                monthName = "February";
-                break;
-            case 3:
-                monthName = "March";
-                break;
-            case 4:
-                monthName = "April";
-                break;
-            case 5:
-                monthName = "May";
-                break;
-            case 6:
-                monthName = "June";
-                break;
-            case 7:
-                monthName = "July";
-                break;
-            case 8:
-                monthName = "August";
-                break;
-            case 9:
-                monthName = "September";
-                break;
-            case 10:
-                monthName = "October";
-                break;
-            case 11:
-                monthName = "November";
-                break;
-            case 12:
-                monthName = "December";
-                break;
-            default:
-
-        }
-        return monthName;
-    }
-}
 /*    public static void main(String[] args) {
 
         //todo  We will run this class to test your codes.
